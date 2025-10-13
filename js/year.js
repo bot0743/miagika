6219f9105c134596b1bb37ab60b44ce57c5e8f24
bot0743 <156990592+bot0743@users.noreply.github.com>
2025-10-13 09:59:41 +0400

    Update year.js

--- js/year.js
+++ js/year.js
@@ -1,1 +1,7 @@
-.
+// Автоматическая замена года в копирайте
+document.addEventListener('DOMContentLoaded', function() {
+    const copyrightElement = document.querySelector('.copyright p');
+    if (copyrightElement) {
+        copyrightElement.innerHTML = copyrightElement.innerHTML.replace('{year}', new Date().getFullYear());
+    }
+});
